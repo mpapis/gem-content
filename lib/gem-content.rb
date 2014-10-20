091d30206b907a140845b428cdc5da845524ea76
Michal Papis <mpapis@gmail.com>
2014-10-20 02:49:50 +0200

    add docs

--- lib/gem-content.rb
+++ lib/gem-content.rb
@@ -6,14 +6,20 @@ See the file LICENSE for copying permission.
 
 require "gem-content/version"
 
+# Find gems with defined content name in metadata and
+# return pull path to the content
 class GemContent
 
+  # @return [String] content name filter
   attr_reader :content_name
 
+  # initializes the filtered content path
+  # @param content_name [String] the path marker to read from gems metadata
   def initialize(content_name)
     @content_name = content_name
   end
 
+  # returns [Array<String>] all gems paths containing requested content
   def get_gem_paths
     active_or_latest_gems_matching.map do |specification|
       File.join(
@@ -25,6 +31,7 @@ class GemContent
 
 private
 
+  # filter active / latest gem versions
   def active_or_latest_gems_matching
     all_gems_matching.group_by(&:name).map do |name, specifications|
       specifications.find(&:activated) or
@@ -32,6 +39,7 @@ private
     end
   end
 
+  # filter gems with content_name metadata
   def all_gems_matching
     all_gems.select do |specification|
       specification.metadata.is_a?(Hash) and
@@ -39,6 +47,7 @@ private
     end
   end
 
+  # get all gem specifications
   def all_gems
     Gem::Specification._all
   end
